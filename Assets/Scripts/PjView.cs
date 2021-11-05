02b7cbc500a8650bd9a9b9e93b3d8af573f062ba
Luis Plata <www.luisplata@gmail.com>
2021-11-05 08:35:54 -0500

    se van a agregar todas las animaciones en un solo controlador porque es la mejor forma de hacerlo de momento

--- Assets/Scripts/PjView.cs
+++ Assets/Scripts/PjView.cs
@@ -1,7 +1,21 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Animations;
+using UnityEngine;
 
 public class PjView : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+    [SerializeField] private AnimationClip ataqueNormal, ataqueEspecial;
+
+    public void Animacion1()
+    {
+        anim.Play(ataqueNormal.name);
+    }
+
+    public void Animacion2()
+    {
+        anim.runtimeAnimatorController.animationClips[0] = ataqueEspecial;
+        anim.Play(ataqueEspecial.name);
+    }
     public void Configurate()
     {
         
