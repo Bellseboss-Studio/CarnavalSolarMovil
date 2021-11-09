a75ae9f6fe789dccf249ac16bb50f50be7ae1262
Luis Plata <www.luisplata@gmail.com>
2021-11-08 20:44:45 -0500

    Adicion del multiplayer; creacion de salas

---
+++ Assets/Scripts/Multiplayer/IMultiplayer.cs
@@ -0,0 +1,9 @@
+﻿public interface IMultiplayer
+{
+    bool EstaListo();
+    void CrearSala(string nombreSala);
+    void UnirseSala(string nombreSala);
+    bool TerminoDeProcesarLaSala();
+    bool FalloAlgo();
+    void ResetFlags();
+}
