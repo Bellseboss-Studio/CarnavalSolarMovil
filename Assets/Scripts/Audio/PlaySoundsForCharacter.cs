69b9463dd21fda7e9f66dbe6a7ae81d20d69c1ad
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-12-27 14:16:37 -0500

    Sounds Potenciador Postmuerte, GeneradorDeMiniEspinas, EnergizadprBAsico

--- Assets/Scripts/Audio/PlaySoundsForCharacter.cs
+++ Assets/Scripts/Audio/PlaySoundsForCharacter.cs
@@ -7,12 +7,15 @@ using UnityEngine.Audio;
 public class PlaySoundsForCharacter : MonoBehaviour, ICheckDependencies
 {
     [SerializeField] private List<AudioClip> m_FsClips;
+    [SerializeField] private List<AudioClip> m_DeathClips;
     [SerializeField] private AudioSource m_AudioSource;
     [SerializeField] private string m_CharacterName;
     [SerializeField] private AudioMixerGroup m_Output;
     
     [Range(0.5f, 1.0f)]
-    [SerializeField] private float m_Volume = 1.0f;
+    [SerializeField] private float m_FsVolume = 7.0f;
+    [Range(0.5f, 1.0f)]
+    [SerializeField] private float m_DeathVolume = 1.0f;
 
     private void Start()
     {
@@ -30,24 +33,12 @@ public class PlaySoundsForCharacter : MonoBehaviour, ICheckDependencies
 
     public void PlayDeathSound()
     {
-
+        SetAudioClipsToPlay(m_DeathClips, m_DeathVolume);
     }
     
     public void PlayFootStepSound()
     {
-        if(m_FsClips.Count > 0)
-        {
-            m_AudioSource.maxDistance = 40;
-            m_AudioSource.volume = m_Volume - Random.Range(0f, 0.4f);
-            m_AudioSource.pitch = Random.Range(0.9f, 1.2f);
-            m_AudioSource.spatialBlend = 0.7f;
-            m_AudioSource.outputAudioMixerGroup = m_Output;
-            m_AudioSource.PlayOneShot(m_FsClips[Random.Range(0, m_FsClips.Count)]);
-        }
-        else
-        {
-            Debug.Log($"No FS Sounds for {this.gameObject.name.Replace("(Clone)", "")}");
-        }
+        SetAudioClipsToPlay(m_FsClips, m_FsVolume);
     }
 
     public void CheckForReferences()
@@ -57,4 +48,23 @@ public class PlaySoundsForCharacter : MonoBehaviour, ICheckDependencies
             m_AudioSource = GetComponent<AudioSource>();
         }
     }
+
+
+    void SetAudioClipsToPlay(List<AudioClip> audioClips, float volume)
+    {
+        if (audioClips.Count > 0)
+        {
+            m_AudioSource.maxDistance = 40;
+            m_AudioSource.volume = volume - Random.Range(0f, 0.4f);
+            m_AudioSource.pitch = Random.Range(0.9f, 1.2f);
+            m_AudioSource.spatialBlend = 0.7f;
+            m_AudioSource.outputAudioMixerGroup = m_Output;
+            m_AudioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count)]);
+        }
+        else
+        {
+            Debug.Log($"No {audioClips.ToString()} Sounds for {this.gameObject.name.Replace("(Clone)", "")}");
+        }
+    }
+
 }
