f2ee4b891619fb5c796df9570315c0f2ffef8de5
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-12-29 22:00:52 -0500

    Added UI Sounds

--- Assets/Scripts/Audio/PlayUISound.cs
+++ Assets/Scripts/Audio/PlayUISound.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class PlayUISound : MonoBehaviour
 {
-    [SerializeField] bool isActive;
-      
+
+
     public void PlayUiSound(string buttonName)
     {
-        if(!isActive)
-        {
-            SfxManager.Instance.PlaySound(buttonName);
-            isActive = true;
-        }else
-        {
-            SfxManager.Instance.PlaySound("GenericButton");
-            isActive = false;
-        }
-        
+
+
+        SfxManager.Instance.PlaySound(buttonName);
+
+
     }
 }
