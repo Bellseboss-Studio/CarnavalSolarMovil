46c819882ca6ac73b3ef173ff8de405f5301d159
Luis Plata <www.luisplata@gmail.com>
2021-12-29 20:34:10 -0500

    culminacion de menu

--- Assets/Scripts/Gameplay/UsoDeCartas/SelectorBaraja.cs
+++ Assets/Scripts/Gameplay/UsoDeCartas/SelectorBaraja.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using ServiceLocatorPath;
 using TMPro;
 using UnityEngine;
@@ -11,7 +12,7 @@ namespace Gameplay.UsoDeCartas
     {
         [SerializeField] private List<string> listaDeIdDeCartasEnBaraja;
         [SerializeField] private string idHeroe;
-        [SerializeField] private Image imageDeFamilia;
+        [SerializeField] private Image imageDeFamilia, seleccion;
         [SerializeField] private Sprite foto;
         [SerializeField] private TextMeshProUGUI textoDeNombreDeFamilia;
         [SerializeField] private string nombreFamilia;
@@ -39,6 +40,30 @@ namespace Gameplay.UsoDeCartas
         public void AniadirBarajaAServiceLocator(SelectorBaraja baraja)
         {
             ServiceLocator.Instance.GetService<IServicioDeBarajasDisponibles>().AniadirBaraja(baraja);
+        }
+
+        public void OscurecerImagen()
+        {
+            var negro = Color.black;
+            negro.a = .2f;
+            seleccion.color = negro;
+        }
+        public void AclararImagen()
+        {
+            seleccion.color = Color.clear;
+        }
+
+        public void MueveteALaDerecha(int distancia)
+        {
+            var sequence = DOTween.Sequence();
+            var tra = GetComponent<RectTransform>();
+            sequence.Insert(0, tra.DOLocalMoveX(tra.localPosition.x + distancia, .4f));
+        }
+        public void MueveteALaIzquierda(int distancia)
+        {
+            var sequence = DOTween.Sequence();
+            var tra = GetComponent<RectTransform>();
+            sequence.Insert(0, tra.DOLocalMoveX(tra.localPosition.x - distancia, .4f));
         }
     }
 }
---
+++ Assets/Scripts/InterfazDeUsuario/ControladorDeHeroesDeslizado.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Gameplay.UsoDeCartas;
+using ServiceLocatorPath;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControladorDeHeroesDeslizado : MonoBehaviour
+{
+    [SerializeField] private List<SelectorBaraja> barajas;
+    [SerializeField] private Button derecha, izquierda, continuar, jugar;
+    [SerializeField] private Animator animator;
+    private int index;
+    private SelectorBaraja _barajaSeleccionada;
+    private bool estaMostrando;
+
+    private void Awake()
+    {
+        var servicioDeBarajasDisponibles = new ServicioDeBarajasDisponibles();
+        ServiceLocator.Instance.RegisterService<IServicioDeBarajasDisponibles>(servicioDeBarajasDisponibles);
+    }
+
+    IEnumerator DiSableAndEnalbe()
+    {
+        derecha.enabled = false;
+        izquierda.enabled = false;
+        yield return new WaitForSeconds(.4f);
+        derecha.enabled = true;
+        izquierda.enabled = true;
+    }
+    private void ContinuarASiguienteEscena()
+    {
+        ServiceLocator.Instance.GetService<IServicioDeBarajasDisponibles>()
+            .SetBarajaSeleccionadaId(_barajaSeleccionada);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void Start()
+    {
+        foreach (var baraja in barajas)
+        {
+            baraja.BotonBarajaSeleccionada.onClick.AddListener(() =>
+            {
+                _barajaSeleccionada = baraja;
+                foreach (var selectorBaraja in barajas)
+                {
+                    selectorBaraja.AclararImagen();
+                }
+                baraja.OscurecerImagen();
+                MostrarBotonDeSeleccionar();
+            });
+        }
+        derecha.onClick.AddListener(() =>
+        {
+            if (index >= 0 && index < barajas.Count -1)
+            {
+                StartCoroutine(DiSableAndEnalbe());
+                foreach (var baraja in barajas)
+                {
+                    baraja.MueveteALaIzquierda(1000);
+                }
+                //_barajaSeleccionada = barajas[index];
+                index++;
+                barajas[index].BotonBarajaSeleccionada.onClick.Invoke();
+            }
+        });
+        izquierda.onClick.AddListener(() =>
+        {
+            if (index > 0 && index <= barajas.Count)
+            {
+                StartCoroutine(DiSableAndEnalbe());
+                foreach (var baraja in barajas)
+                {
+                    baraja.MueveteALaDerecha(1000);
+                }
+                index--;
+                barajas[index].BotonBarajaSeleccionada.onClick.Invoke();
+            }
+        });
+        
+        continuar.onClick.AddListener(ContinuarASiguienteEscena);
+        
+        jugar.onClick.AddListener(() =>
+        {
+            animator.SetBool("jugar", true);
+        });
+        
+        barajas[0].BotonBarajaSeleccionada.onClick.Invoke();
+    }
+
+    private void MostrarBotonDeSeleccionar()
+    {
+        if(estaMostrando) return;
+        var sequence = DOTween.Sequence();
+        var tra = continuar.gameObject.GetComponent<RectTransform>();
+        sequence.Insert(0, tra.DOLocalMoveX(tra.localPosition.x + 1000, .4f));
+        estaMostrando = true;
+    }
+}
