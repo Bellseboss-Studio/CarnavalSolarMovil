f6d7d46babe54f2f617b2d24d054d530f92e0bfc
diosito Gonzalez Marquez <bolo402402@gmail.comname>
2022-04-19 11:44:25 -0500

    photon aniadido

---
+++ Assets/Scripts/NewMultiplayer/PhotonLobby.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NewMultiplayer
+{
+    public class PhotonLobby : MonoBehaviourPunCallbacks
+    {
+        private TypedLobby customLobby = new TypedLobby("customLobby", LobbyType.Default);
+        
+        [SerializeField] private Button playOnlineButton;
+        
+        
+        private void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings();
+            playOnlineButton.onClick.AddListener(ConectarAlLobby);
+        }
+
+        private void ConectarAlLobby()
+        {
+            PhotonNetwork.JoinLobby(customLobby);
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+            Debug.Log("En Linea");
+        }
+
+        public override void OnJoinedLobby()
+        {
+            base.OnJoinedLobby();
+            Debug.Log($"Conectado al lobby ${PhotonNetwork.CurrentLobby.Name}");
+        }
+    }
+}
